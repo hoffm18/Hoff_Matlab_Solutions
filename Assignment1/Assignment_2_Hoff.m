a522f4667de0e8cee219e17877f723ec062dcba0
Michael Hoff <hoffm18@students.ecu.edu>
2018-09-12 15:13:12 -0400

    Assignment2Hoff

---
+++ Assignment1/Assignment_2_Hoff.m
@@ -0,0 +1,60 @@
+%Michael Hoff
+%September 13th
+%This assignment is exercises from chapters 1-3 in our textbook
+
+%%Chapter 1 Exercises
+
+%1.)
+Copper=63.55
+
+%2.)
+
+myage=22
+myage-2
+myage+1
+
+%3.)
+namelengthmax
+%63
+
+%4.)
+P=4
+O=64
+who
+whos
+clear P
+who
+whos
+
+%5.)
+intmin('uint32')
+intmax('uint32')
+intmin('uint64')
+intmax('uint64')
+%6.)
+dec=.500
+deci = int32(dec);
+
+%11.)
+pounds=10
+kilos = pounds/2.2
+
+%12.)
+ftemp=50
+ctemp = (ftemp-32)*5/9
+
+%13.)
+cent=10
+inch = cent/2.54
+
+%14.)
+
+
+%15.)
+
+
+%22.)
+%Uppercase letters come before lowercase letters. 
+
+%24.)
+
